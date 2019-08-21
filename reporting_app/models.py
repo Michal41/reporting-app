e91aac559a19bd8a3111eecfe1a15d6db6bbb1d5
Fineax <michal.kanarek@gmail.com>
2019-08-21 21:32:44 +0200

    Add panel that contains all drivers info

--- reporting_app/models.py
+++ reporting_app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Action(models.Model):
@@ -13,3 +14,16 @@ class Action(models.Model):
     def __str__(self):
         return self.driver.username +" route " + self.route
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_driver = models.BooleanField(default=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- reporting_app/views.py
+++ reporting_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Action
-
+from django.contrib.auth.models import User
+from .models import Profile
 def driver_panel(request):
     road_list = ["956", "23", "234", "22"]
     context = {"road_list": road_list,
@@ -11,6 +12,14 @@ def driver_panel(request):
                "title": "Driver Panel"}
     return render(request, "reporting_app/driver_panel.html", context)
 
+
+def main_panel(request):
+    drivers_stats = [(Action.objects.filter(driver=x.user).last(),i) for x,i in zip(Profile.objects.filter(is_driver=True), range(1,1+len(Profile.objects.filter(is_driver=True))))]
+    print(drivers_stats)
+
+    context = {"drivers_stats":drivers_stats,
+               }
+    return render(request, "reporting_app/main_panel.html", context)
 
 def driver_action_start(request):
     if (request.method == "POST") and Action.objects.all().last().is_active==False:
